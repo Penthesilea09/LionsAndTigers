5dce5c73292a1cb33ad228cd85a03cf2abf5d0fc
Victor Martinez <victormartinez@Mac-Pro-de-Victor.local>
2014-09-29 14:12:54 +0200

    Initial Commit

---
+++ 03 Lions And TigersTests/_3_Lions_And_TigersTests.swift
@@ -0,0 +1,36 @@
+//
+//  _3_Lions_And_TigersTests.swift
+//  03 Lions And TigersTests
+//
+//  Created by Victor Martinez on 29/09/14.
+//  Copyright (c) 2014 Victor Mart. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class _3_Lions_And_TigersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
